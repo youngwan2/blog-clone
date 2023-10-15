56e9810694a5f0ac1cfe9312228c504e6ee7b20e
KYW <qodna25@gmail.com>
2023-10-15 17:33:51 +0900

    마크다운 이미지 추가

--- src/app/posts/[path]/page.tsx
+++ src/app/posts/[path]/page.tsx
@@ -1,7 +1,6 @@
-
+import Image from "next/image";
 import MarkdownView from "@/components/MarkdownView";
-import { getPostMarkdown } from "@/service/posts";
-
+import { Post, getPostMarkdown } from "@/service/posts";
 
 type Props = {
   params: {
@@ -12,11 +11,25 @@ export default async function PostsDetail({ params }: Props) {
   console.log(params.path);
 
   const { metaData, data } = await getPostMarkdown(params.path);
+  const { title, description, date, category, path } = metaData as Post;
 
   return (
-    <section>
-      <h2>{metaData?.title}</h2>
-      <MarkdownView markdown={data}/>
+    <section className="w-full px-16 pt-5">
+      <article className="max-w-3xl mx-auto">
+        <Image
+          className="mx-auto"
+          src={`/images/${path}.png`}
+          alt="postImage"
+          width={720}
+          height={420}
+        />
+        <p className="text-right">{date}</p>
+        <h2 className="text-xl font-bold py-1.5">{title}</h2>
+        <p>{description}</p>
+        <span className="bg-orange-400 p-1 rounded-md inline-block">{category}</span>
+      </article>
+      <hr />
+      <MarkdownView markdown={data} />
     </section>
   );
 }
--- src/components/MarkdownView.tsx
+++ src/components/MarkdownView.tsx
@@ -1,13 +1,47 @@
+"use client";
+import Image from "next/image";
+
 import Markdown from "react-markdown";
-import  remarkGfm  from  'remark-gfm' 
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-export default function MarkdownView({markdown}:{markdown:string}) {
-
-
-
-    return (
-        <Markdown className={"prose lg:prose-xl prose-neutral mx-auto"} remarkPlugins={[remarkGfm]}>{markdown}</Markdown>
-        
-    )
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+export default function MarkdownView({ markdown }: { markdown: string }) {
+  return (
+    // children 속성에 값을 맵핑하지 말고, 컴포넌트 태그 사이에 {data} 형식으로 넣어주어야 한다.
+    <Markdown
+      className={"prose lg:prose-xl mx-auto"}
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code(props) {
+          const { children, className, node, ...rest } = props;
+          const match = /language-(\w+)/.exec(className || "");
+          return match ? (
+            <SyntaxHighlighter
+              {...rest}
+              ref={null}
+              style={dark}
+              language={match[1]}
+              PreTag="div"
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          );
+        },
+        img: (image) => (
+          <Image
+            src={image.src || ""}
+            alt={image.alt || ""}
+            width={500}
+            height={300}
+          />
+        ),
+      }}
+    >
+      {markdown}
+    </Markdown>
+  );
 }
